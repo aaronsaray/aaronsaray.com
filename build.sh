da5e56b91ba43c4a3fabcce971b6b045893bd7b5
Aaron Saray <me@aaronsaray.com>
2017-04-15 16:41:19 -0500

    Added environment

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-jekyll build && terminal-notifier -title "Blog Built" -message "Jekyll has built the blog successfully"
+JEKYLL_ENV=production jekyll build && terminal-notifier -title "Blog Built" -message "Jekyll has built the blog successfully"
 git add *
 git commit -a -m "Building blog"
 git push
